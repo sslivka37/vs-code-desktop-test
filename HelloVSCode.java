e205e8fe972ffcae60f57c9245f75ebe67d99dc6
sslivka37 <sslivka37@gmail.com>
2019-04-24 22:23:32 -0400

    First commit, making sure able to set up and push to remote repositories using VS code projects

---
+++ HelloVSCode.java
@@ -0,0 +1,6 @@
+public class HelloVSCode{
+    public static void main(String[] args) {
+        String test = "This is my first project using VS Code/Java on my home computer!";
+        System.out.println(test);
+    }
+}
